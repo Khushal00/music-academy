8d4ff882181ab8e431bdee165b405a98244bd154
Khushal00 <khushalrathore1003@gmail.com>
2024-11-04 17:43:42 +0530

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <div className="text-2xl text-center">
-      chai aur code
       </div>
       <HeroSection />
       <FeaturedCourses />
